f4d089d1a1ba3ceee5f195507cab119c2130fd5f
JAFETW <162158614+JAFETW@users.noreply.github.com>
2024-07-16 11:42:55 -0600

    subiendo un archivo

---
+++ src/probandogithub.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class probandogithub {
+    public static void main(String[] args) {
+         Scanner lea=new Scanner(System.in);
+    }
+}
